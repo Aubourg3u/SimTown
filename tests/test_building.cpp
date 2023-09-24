13971885110fa293e2ffb7376dde55b1a3b6268e
Elysio <nathan.aubourg@laposte.net>
2023-09-24 03:11:18 +0200

    Deleted package.json

---
+++ tests/test_building.cpp
@@ -0,0 +1,21 @@
+#include<class/building.h>
+
+int main(int argc, char** argv) {
+    
+    Tower Tower1(1, 2);
+    Tower Tower2(2, 4);
+
+    Tower1 = Tower2;
+
+    House House1(1, 2);
+    House House2(2, 1);
+
+    House2 = House1;
+
+    TownHall TH1(0);
+    TownHall TH2(1);
+
+    TH1 = TH2;
+
+    return 0; // Normal program termination.
+}
